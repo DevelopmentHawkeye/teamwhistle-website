d6fd976ce0f3bd4233057842a00660bfa0eccb2d
Hawkeye Development <developmenthawkeye@gmail.com>
2026-01-01 13:19:48 +0100

    Add homeage (missing images)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 
 import { PrivacyComponent } from './privacy/privacy.component';
+import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
-    {path: 'privacy', component: PrivacyComponent}
+    {path: 'privacy', component: PrivacyComponent},
+    {path: '', component: HomeComponent},
 ];
